c886cfa95b21ae53192b9d73a349d8cca9740948
Karl Office <karl.bittner@boku.ac.at>
2026-05-11 10:03:37 +0200

    fix: disable backface normal flipping in leaf shader

--- src/lib/tree.js
+++ src/lib/tree.js
@@ -487,6 +487,8 @@ export class Tree extends THREE.Group {
       const n = new THREE.Vector3(0, 0, 1).applyEuler(orientation);
       const vertexNormalWeight = 0.5;
 
+      // The normal vectors are an average of the direction of the leaf and the directions to the individual vertices.
+      // This creates a nice rounded shape while maintaining the canopy shape as a whole.
       let n1 = new THREE.Vector3().copy(n).add(v[0]).sub(origin).normalize();
       let n2 = new THREE.Vector3().copy(n).add(v[1]).sub(origin).normalize();
       let n3 = new THREE.Vector3().copy(n).add(v[2]).sub(origin).normalize();
@@ -796,6 +798,77 @@ transferNormals(targetGeo, sourceGeo) {
         gl_Position = projectionMatrix * mvPosition;
         `
       );
+
+      // Remove the backface normal flip line from "normal_fragment_begin" because we use custom normals
+      shader.fragmentShader = shader.fragmentShader.replace("#include <normal_fragment_begin>", `
+        float faceDirection = gl_FrontFacing ? 1.0 : - 1.0;
+
+        #ifdef FLAT_SHADED
+
+          vec3 fdx = dFdx( vViewPosition );
+          vec3 fdy = dFdy( vViewPosition );
+          vec3 normal = normalize( cross( fdx, fdy ) );
+
+        #else
+
+          vec3 normal = normalize( vNormal );
+
+        #endif
+
+        #if defined( USE_NORMALMAP_TANGENTSPACE ) || defined( USE_CLEARCOAT_NORMALMAP ) || defined( USE_ANISOTROPY )
+
+          #ifdef USE_TANGENT
+
+            mat3 tbn = mat3( normalize( vTangent ), normalize( vBitangent ), normal );
+
+          #else
+
+            mat3 tbn = getTangentFrame( - vViewPosition, normal,
+            #if defined( USE_NORMALMAP )
+              vNormalMapUv
+            #elif defined( USE_CLEARCOAT_NORMALMAP )
+              vClearcoatNormalMapUv
+            #else
+              vUv
+            #endif
+            );
+
+          #endif
+
+          #if defined( DOUBLE_SIDED ) && ! defined( FLAT_SHADED )
+
+            tbn[0] *= faceDirection;
+            tbn[1] *= faceDirection;
+
+          #endif
+
+        #endif
+
+        #ifdef USE_CLEARCOAT_NORMALMAP
+
+          #ifdef USE_TANGENT
+
+            mat3 tbn2 = mat3( normalize( vTangent ), normalize( vBitangent ), normal );
+
+          #else
+
+            mat3 tbn2 = getTangentFrame( - vViewPosition, normal, vClearcoatNormalMapUv );
+
+          #endif
+
+          #if defined( DOUBLE_SIDED ) && ! defined( FLAT_SHADED )
+
+            tbn2[0] *= faceDirection;
+            tbn2[1] *= faceDirection;
+
+          #endif
+
+        #endif
+
+        // non perturbed normal for clearcoat among others
+
+        vec3 nonPerturbedNormal = normal;
+      `);
 
       mat.userData.shader = shader;
     };
